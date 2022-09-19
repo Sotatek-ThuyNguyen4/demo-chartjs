9b86a558193d850a17e9fe48a5f7a6eadef9fe0c
Sotatek-ThuyNguyen4 <thuy.nguyen4@sotatek.com>
2022-09-19 16:49:39 +0700

    custom line chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CustomLineChart from "./components/customLineChart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mx-auto py-4">
+        <h1>Demo App</h1>
+        <div className="mt-5 py-4 row">
+          <div className="col col-md-6">
+            <div className="card">
+              <div className="card-header">Balance Overview</div>
+              <div className="card-body">
+                <CustomLineChart />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/customLineChart/index.js
@@ -0,0 +1,144 @@
+import React from "react";
+import styles from "./styles.module.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "bottom",
+      labels: {
+        usePointStyle: true,
+      },
+    },
+    title: {
+      display: false,
+    },
+    tooltip: {
+      usePointStyle: true,
+      callbacks: {
+        label: function (context) {
+          let label = context.dataset.label || "";
+
+          if (label) {
+            label += ": ";
+          }
+          if (context.parsed.y !== null) {
+            label += "$" + context.parsed.y + "k";
+          }
+          return label;
+        },
+      },
+    },
+  },
+  scales: {
+    x: {
+      display: true,
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      beginAtZero: true,
+      display: true,
+      borderType: "solid",
+      lineWidth: 0,
+      scaleLabel: {
+        show: true,
+      },
+      ticks: {
+        fontColor: "#8f9092",
+        callback: function (value) {
+          return "$" + value + "k";
+        },
+      },
+    },
+  },
+  interaction: {
+    intersect: false,
+    mode: "index",
+    axis: "x",
+  },
+};
+
+const labels = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+export const data = {
+  labels,
+  responsive: true,
+  datasets: [
+    {
+      label: "Revenue",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 500 })),
+      tension: 0.5,
+      borderColor: "#0ab39c",
+      backgroundColor: "#0ab39c",
+      borderWidth: 2,
+      fill: {
+        target: "origin",
+        above: "rgb(10 179 156 / 6%)",
+        below: "rgb(10 179 156 / 6%)",
+      },
+      pointRadius: 0,
+    },
+    {
+      label: "Expenses",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 500 })),
+      tension: 0.5,
+      borderColor: "#f06548",
+      backgroundColor: "#f06548",
+      borderWidth: 2,
+      fill: {
+        target: "origin",
+        above: "rgb(240 101 72 / 6%)",
+        below: "rgb(240 101 72 / 6%)",
+      },
+      pointRadius: 0,
+    },
+  ],
+  stroke: {
+    curve: "smooth",
+    width: 2,
+  },
+};
+
+const CustomLineChart = () => {
+  return <Line options={options} data={data} />;
+};
+
+export default CustomLineChart;
